34c41d84ee83446c065247492ca7f2aa01287df1
Yeray Hernandez <alu0100435771@ull.edu.es>
2013-10-09 10:26:17 +0100

    Añadiendo codigo

--- matriz.rb
+++ matriz.rb
@@ -1,3 +1,30 @@
-!# usr/bin/rub
+#! /usr/bin/ruby
 
-Practica 4 
+puts "                                       "
+puts "Practica 4: Suma y producto de Matrices"
+puts "---------------------------------------"
+#Inicializando matrices 
+a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] 
+b = [[9, 8, 7], [6, 5, 4], [3, 2, 1]]   
+c = [[0,0,0],[0,0,0],[0,0,0]]                          
+
+def mostrar_matrices (a, b)
+m, n = a.size, a[1].size
+for i in 0...m do
+    for j in 0...n do
+      print a[i][j]
+      print " "
+    end
+    puts ""
+  end
+puts"             "
+for i in 0...m do
+    for j in 0...n do
+      print b[i][j]
+      print " "
+    end
+    puts ""
+  end
+puts "              "
+end
+
